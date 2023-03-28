08f27bddb447483a01fc9f9891dba81bbdaf220e
Yuval Marcus <ymarcus93@gmail.com>
2023-03-28 00:46:39 -0400

    Modify attribute tests for environment resource

--- bastionzero/environment/resource_environment_test.go
+++ bastionzero/environment/resource_environment_test.go
@@ -16,7 +16,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 )
 
-func TestAccEnvironment_Basic(t *testing.T) {
+func TestAccEnvironment_CreateAndUpdate(t *testing.T) {
 	var env environments.Environment
 	name := bztftest.RandomTestName()
 	resourceName := "bastionzero_environment.env"
@@ -26,6 +26,7 @@ func TestAccEnvironment_Basic(t *testing.T) {
 		ProtoV6ProviderFactories: bztftest.TestProtoV6ProviderFactories,
 		CheckDestroy:             testAccCheckEnvironmentDestroy,
 		Steps: []resource.TestStep{
+			// Create environment
 			{
 				Config: environmentResourceTFConfig("env", &environmentResourceOptions{
 					Name:        &name,
@@ -44,6 +45,61 @@ func TestAccEnvironment_Basic(t *testing.T) {
 					resource.TestMatchResourceAttr(resourceName, "id", bztftest.ExpectedIDRegEx()),
 					resource.TestMatchResourceAttr(resourceName, "organization_id", bztftest.ExpectedIDRegEx()),
 					resource.TestMatchResourceAttr(resourceName, "time_created", bztftest.ExpectedTimestampRegEx()),
+				),
+			},
+			// Modify description and cleanup timeout
+			{
+				Config: environmentResourceTFConfig("env", &environmentResourceOptions{
+					Name:                       &name,
+					Description:                bastionzero.PtrTo("new-desc"),
+					OfflineCleanupTimeoutHours: bastionzero.PtrTo(3000)}),
+				Check: resource.ComposeTestCheckFunc(
+					bztftest.TestAccCheckEnvironmentExists(resourceName, &env),
+					testAccCheckEnvironmentAttributes(&env, &environmentResourceOptions{
+						Name:                       &name,
+						Description:                bastionzero.PtrTo("new-desc"),
+						OfflineCleanupTimeoutHours: bastionzero.PtrTo(3000)}),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "description", "new-desc"),
+					resource.TestCheckResourceAttr(resourceName, "offline_cleanup_timeout_hours", strconv.Itoa(3000)),
+					resource.TestCheckResourceAttr(resourceName, "is_default", "false"),
+					resource.TestCheckResourceAttr(resourceName, "targets.%", "0"),
+					resource.TestMatchResourceAttr(resourceName, "id", bztftest.ExpectedIDRegEx()),
+					resource.TestMatchResourceAttr(resourceName, "organization_id", bztftest.ExpectedIDRegEx()),
+					resource.TestMatchResourceAttr(resourceName, "time_created", bztftest.ExpectedTimestampRegEx()),
+				),
+			},
+		},
+	})
+}
+
+func TestAccEnvironment_UpdateName(t *testing.T) {
+	var afterCreate, afterUpdate environments.Environment
+	name := bztftest.RandomTestName()
+	name2 := bztftest.RandomTestName()
+	resourceName := "bastionzero_environment.env"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { bztftest.TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: bztftest.TestProtoV6ProviderFactories,
+		CheckDestroy:             testAccCheckEnvironmentDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: environmentResourceTFConfig("env", &environmentResourceOptions{Name: &name}),
+				Check: resource.ComposeTestCheckFunc(
+					bztftest.TestAccCheckEnvironmentExists(resourceName, &afterCreate),
+					testAccCheckEnvironmentAttributes(&afterCreate, &environmentResourceOptions{Name: &name}),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+				),
+			},
+			// Change name should force re-create the resource
+			{
+				Config: environmentResourceTFConfig("env", &environmentResourceOptions{Name: &name2}),
+				Check: resource.ComposeTestCheckFunc(
+					bztftest.TestAccCheckEnvironmentExists(resourceName, &afterUpdate),
+					testAccCheckEnvironmentAttributes(&afterUpdate, &environmentResourceOptions{Name: &name2}),
+					resource.TestCheckResourceAttr(resourceName, "name", name2),
+					testAccCheckEnvironmentRecreated(t, &afterCreate, &afterUpdate),
 				),
 			},
 		},
@@ -99,6 +155,15 @@ func testAccCheckEnvironmentDestroy(s *terraform.State) error {
 	}
 
 	return nil
+}
+
+func testAccCheckEnvironmentRecreated(t *testing.T, before, after *environments.Environment) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		if before.ID == after.ID {
+			t.Fatalf("Expected change of environment IDs, but both were %v", before.ID)
+		}
+		return nil
+	}
 }
 
 func testAccCheckEnvironmentAttributes(env *environments.Environment, opts *environmentResourceOptions) resource.TestCheckFunc {
