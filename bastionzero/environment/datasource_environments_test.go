f30de6ce79f38627cfdd7a8a9d8ec89026ab1193
Yuval Marcus <ymarcus93@gmail.com>
2023-03-30 18:16:44 -0400

    attempt diff design for list data source

--- bastionzero/environment/datasource_environments_test.go
+++ bastionzero/environment/datasource_environments_test.go
@@ -1,36 +1,56 @@
 package environment_test
 
-// func TestAccDataSourceEnvironments_Basic(t *testing.T) {
-// 	// Create random env name
-// 	name := acctest.RandomName()
+import (
+	"context"
+	"testing"
 
-// 	resourceConfig := environmentResourceTFConfig("env", &environmentResourceOptions{Name: &name})
-// 	dataSourceConfig := `data "bastionzero_environments" "envs" {}`
+	"github.com/bastionzero/bastionzero-sdk-go/bastionzero/service/environments"
+	"github.com/bastionzero/terraform-provider-bastionzero/internal/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
 
-// 	resource.Test(t, resource.TestCase{
-// 		PreCheck:                 func() { acctest.PreCheck(context.Background(), t) },
-// 		ProtoV6ProviderFactories: acctest.TestProtoV6ProviderFactories,
-// 		Steps: []resource.TestStep{
-// 			{
-// 				Config: resourceConfig,
-// 			},
-// 			{
-// 				Config: resourceConfig + dataSourceConfig,
-// 				Check: resource.ComposeTestCheckFunc(
-// 					// Not much else we can do unless we run against an isolated
-// 					// BastionZero backend. Also, I tried using local variable
-// 					// with filter, but it doesn't seem to be well supported in
-// 					// the terraform-plugin-testing framework.
-// 					//
-// 					// We can probably test more attributes if we force TF
-// 					// version 1.4.0 and use `terraform_data` resource (coupled
-// 					// with local variable that filters for env with name), but
-// 					// don't want to add TF specific tests until this issue is
-// 					// resolved:
-// 					// https://github.com/hashicorp/terraform-plugin-testing/issues/68
-// 					acctest.CheckListHasElements("data.bastionzero_environments.envs", "environments"),
-// 				),
-// 			},
-// 		},
-// 	})
-// }
+func TestAccDataSourceEnvironments_Basic(t *testing.T) {
+	ctx := context.Background()
+	rName := acctest.RandomName()
+	resourceName := "bastionzero_environment.test"
+	dataSourceName := "data.bastionzero_environments.test"
+	var env environments.Environment
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ProtoV6ProviderFactories: acctest.TestProtoV6ProviderFactories,
+		CheckDestroy:             testAccCheckEnvironmentDestroy,
+		Steps: []resource.TestStep{
+			// First create resource
+			{
+				Config: testAccEnvironmentConfigName(rName),
+			},
+			{
+				Config: acctest.ConfigCompose(testAccEnvironmentConfigName(rName), testAccEnvironmentsDataSourceConfig()),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckEnvironmentExists(resourceName, &env),
+					// Not much else we can do unless we run against an isolated
+					// BastionZero backend. Also, I tried using local variable
+					// with filter, but it doesn't seem to be well supported in
+					// the terraform-plugin-testing framework.
+					//
+					// We can probably test more attributes if we force TF
+					// version 1.4.0 and use `terraform_data` resource (coupled
+					// with local variable that filters for env with name), but
+					// don't want to add TF specific tests until this issue is
+					// resolved:
+					// https://github.com/hashicorp/terraform-plugin-testing/issues/68
+					acctest.CheckListHasElements("data.bastionzero_environments.envs", "environments"),
+					resource.TestCheckTypeSetElemAttrPair(resourceName, "name", dataSourceName, "environments.*"),
+				),
+			},
+		},
+	})
+}
+
+func testAccEnvironmentsDataSourceConfig() string {
+	return `
+data "bastionzero_environments" "test" {
+}
+`
+}
