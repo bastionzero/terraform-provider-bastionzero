b3a1739d114470d140a1ee4d85234395016eaed8
Yuval Marcus <ymarcus93@gmail.com>
2023-04-02 02:24:21 -0400

    Add list data source acctest for tc policy

---
+++ bastionzero/policy/targetconnect/datasource_targetconnectpolicies_test.go
@@ -0,0 +1,49 @@
+package targetconnect_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/bastionzero/bastionzero-sdk-go/bastionzero/service/policies"
+	"github.com/bastionzero/bastionzero-sdk-go/bastionzero/service/policies/verbtype"
+	"github.com/bastionzero/terraform-provider-bastionzero/internal/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccDataSourceTargetConnectPolicies_Basic(t *testing.T) {
+	ctx := context.Background()
+	rName := acctest.RandomName()
+	resourceName := "bastionzero_targetconnect_policies.test"
+	dataSourceName := "data.bastionzero_targetconnect_policies.test"
+	var policy policies.TargetConnectPolicy
+
+	resourcePolicy := testAccTargetConnectPolicyConfigBasic(rName, []string{"foo"}, []string{string(verbtype.Shell)})
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ProtoV6ProviderFactories: acctest.TestProtoV6ProviderFactories,
+		CheckDestroy:             testAccCheckTargetConnectPolicyDestroy,
+		Steps: []resource.TestStep{
+			// First create resource
+			{
+				Config: resourcePolicy,
+			},
+			// Then check that the list data source contains the policy we
+			// created above
+			{
+				Config: acctest.ConfigCompose(resourcePolicy, testAccTargetConnectPoliciesDataSourceConfig()),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckTargetConnectPolicyExists(resourceName, &policy),
+					acctest.CheckListHasElements(dataSourceName, "policies"),
+					acctest.CheckTypeSetElemNestedAttrsFromResource(resourceName, []string{}, dataSourceName, "policies.*"),
+				),
+			},
+		},
+	})
+}
+
+func testAccTargetConnectPoliciesDataSourceConfig() string {
+	return `
+data "bastionzero_targetconnect_policies" "test" {
+}
+`
+}
