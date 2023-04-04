d579d97a603e03fd86faab3275634a4d021b5075
Yuval Marcus <ymarcus93@gmail.com>
2023-04-04 15:00:29 -0400

    fixup! Add acceptance test for service accounts data source

--- bastionzero/serviceaccount/datasource_serviceaccounts_test.go
+++ bastionzero/serviceaccount/datasource_serviceaccounts_test.go
@@ -12,6 +12,32 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
+func getValuesCheckMap(serviceAccount *serviceaccounts.ServiceAccount) map[string]string {
+	valuesCheckMap := map[string]string{
+		"created_by":       serviceAccount.CreatedBy,
+		"email":            serviceAccount.Email,
+		"enabled":          fmt.Sprintf("%t", serviceAccount.Enabled),
+		"external_id":      serviceAccount.ExternalID,
+		"id":               serviceAccount.ID,
+		"is_admin":         fmt.Sprintf("%t", serviceAccount.IsAdmin),
+		"jwks_url":         serviceAccount.JwksURL,
+		"jwks_url_pattern": serviceAccount.JwksURLPattern,
+		"organization_id":  serviceAccount.OrganizationID,
+		"time_created":     serviceAccount.TimeCreated.UTC().Format(time.RFC3339),
+		"type":             string(subjecttype.ServiceAccount),
+	}
+
+	if serviceAccount.LastLogin != nil {
+		valuesCheckMap["last_login"] = serviceAccount.LastLogin.UTC().Format(time.RFC3339)
+	} else {
+		// TestCheckTypeSetElemNestedAttrs() will check to see that this
+		// attribute is unset (null)
+		valuesCheckMap["last_login"] = ""
+	}
+
+	return valuesCheckMap
+}
+
 func TestAccServiceAccountsDataSource_Basic(t *testing.T) {
 	ctx := context.Background()
 	dataSourceName := "data.bastionzero_service_accounts.test"
@@ -28,20 +54,7 @@ func TestAccServiceAccountsDataSource_Basic(t *testing.T) {
 				Config: testAccServiceAccountsDataSourceConfig(),
 				// Check that the service account we queried for is returned in
 				// the list
-				Check: resource.TestCheckTypeSetElemNestedAttrs(dataSourceName, "service_accounts.*", map[string]string{
-					"created_by":       serviceAccount.CreatedBy,
-					"email":            serviceAccount.Email,
-					"enabled":          fmt.Sprintf("%t", serviceAccount.Enabled),
-					"external_id":      serviceAccount.ExternalID,
-					"id":               serviceAccount.ID,
-					"is_admin":         fmt.Sprintf("%t", serviceAccount.IsAdmin),
-					"jwks_url":         serviceAccount.JwksURL,
-					"jwks_url_pattern": serviceAccount.JwksURLPattern,
-					"last_login":       serviceAccount.LastLogin.UTC().Format(time.RFC3339),
-					"organization_id":  serviceAccount.OrganizationID,
-					"time_created":     serviceAccount.TimeCreated.UTC().Format(time.RFC3339),
-					"type":             string(subjecttype.ServiceAccount),
-				}),
+				Check: resource.TestCheckTypeSetElemNestedAttrs(dataSourceName, "service_accounts.*", getValuesCheckMap(serviceAccount)),
 			},
 		},
 	})
