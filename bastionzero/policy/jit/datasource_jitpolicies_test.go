5d072545d34ec29fe61bbf4a500c843a5febd15e
Yuval Marcus <ymarcus93@gmail.com>
2023-04-04 12:56:37 -0400

    Add JIT data source acceptance tests

---
+++ bastionzero/policy/jit/datasource_jitpolicies_test.go
@@ -0,0 +1,134 @@
+package jit_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/bastionzero/bastionzero-sdk-go/bastionzero/service/policies"
+	"github.com/bastionzero/terraform-provider-bastionzero/bastionzero/policy"
+	"github.com/bastionzero/terraform-provider-bastionzero/internal/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccJITPoliciesDataSource_Basic(t *testing.T) {
+	ctx := context.Background()
+	rName := acctest.RandomName()
+	resourceName := "bastionzero_jit_policy.test"
+	dataSourceName := "data.bastionzero_jit_policies.test"
+	var policy policies.JITPolicy
+	tcPolicy, kubePolicy, proxyPolicy := getChildPoliciesOrSkip(ctx, t)
+
+	resourcePolicy := testAccJITPolicyConfigBasic(rName, []string{tcPolicy.ID, kubePolicy.ID, proxyPolicy.ID})
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: acctest.TestProtoV6ProviderFactories,
+		CheckDestroy:             testAccCheckJITPolicyDestroy,
+		Steps: []resource.TestStep{
+			// First create resource
+			{
+				Config: resourcePolicy,
+			},
+			// Then check that the list data source contains the policy we
+			// created above
+			{
+				Config: acctest.ConfigCompose(resourcePolicy, testAccJITPoliciesDataSourceConfig()),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckJITPolicyExists(resourceName, &policy),
+					acctest.CheckListOrSetHasElements(dataSourceName, "policies"),
+					acctest.CheckTypeSetElemNestedAttrsFromResource(resourceName, []string{}, dataSourceName, "policies.*"),
+				),
+			},
+		},
+	})
+}
+
+func testAccJITPoliciesDataSourceConfig() string {
+	return `
+data "bastionzero_jit_policies" "test" {
+}
+`
+}
+
+func TestAccJITPoliciesDataSource_FilterSubjects(t *testing.T) {
+	ctx := context.Background()
+	rName := acctest.RandomName()
+	dataSourceName := "data.bastionzero_jit_policies.test"
+	subject := new(policies.Subject)
+	tcPolicy, kubePolicy, proxyPolicy := getChildPoliciesOrSkip(ctx, t)
+
+	acctest.FindNUsersOrSkip(t, subject)
+
+	resourcePolicy := testAccJITPolicyConfigSubjects(rName, []string{tcPolicy.ID, kubePolicy.ID, proxyPolicy.ID}, policy.FlattenPolicySubjects(ctx, []policies.Subject{*subject}))
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: acctest.TestProtoV6ProviderFactories,
+		CheckDestroy:             testAccCheckJITPolicyDestroy,
+		Steps: []resource.TestStep{
+			// First create resource
+			{
+				Config: resourcePolicy,
+			},
+			// Then check that we can filter for it
+			{
+				Config: acctest.ConfigCompose(resourcePolicy, testAccJITPoliciesDataSourceConfigFilterSubjects([]string{subject.ID})),
+				Check: resource.ComposeTestCheckFunc(
+					acctest.CheckListOrSetHasElements(dataSourceName, "policies"),
+					acctest.CheckAllPoliciesHaveSubjectID(dataSourceName, subject.ID),
+				),
+			},
+			// Cannot do zero matches test because must provide valid subject
+			// UUID which we can't guarantee. Can add later if we remove backend
+			// restriction that subject ID must exist
+		},
+	})
+}
+
+func testAccJITPoliciesDataSourceConfigFilterSubjects(subjectIDs []string) string {
+	return fmt.Sprintf(`
+data "bastionzero_jit_policies" "test" {
+  filter_subjects = %[1]s
+}
+`, acctest.ToTerraformStringList(subjectIDs))
+}
+
+func TestAccJITPoliciesDataSource_FilterGroups(t *testing.T) {
+	ctx := context.Background()
+	rName := acctest.RandomName()
+	dataSourceName := "data.bastionzero_jit_policies.test"
+	group := new(policies.Group)
+	tcPolicy, kubePolicy, proxyPolicy := getChildPoliciesOrSkip(ctx, t)
+
+	acctest.FindNGroupsOrSkip(t, group)
+
+	resourcePolicy := testAccJITPolicyConfigGroups(rName, []string{tcPolicy.ID, kubePolicy.ID, proxyPolicy.ID}, policy.FlattenPolicyGroups(ctx, []policies.Group{*group}))
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: acctest.TestProtoV6ProviderFactories,
+		CheckDestroy:             testAccCheckJITPolicyDestroy,
+		Steps: []resource.TestStep{
+			// First create resource
+			{
+				Config: resourcePolicy,
+			},
+			// Then check that we can filter for it
+			{
+				Config: acctest.ConfigCompose(resourcePolicy, testAccJITPoliciesDataSourceConfigFilterGroups([]string{group.ID})),
+				Check: resource.ComposeTestCheckFunc(
+					acctest.CheckListOrSetHasElements(dataSourceName, "policies"),
+					acctest.CheckAllPoliciesHaveGroupID(dataSourceName, group.ID),
+				),
+			},
+			// Zero matches
+			{
+				Config: testAccJITPoliciesDataSourceConfigFilterGroups([]string{"foo"}),
+				Check:  resource.TestCheckResourceAttr(dataSourceName, "policies.#", "0"),
+			},
+		},
+	})
+}
+
+func testAccJITPoliciesDataSourceConfigFilterGroups(groupIDs []string) string {
+	return fmt.Sprintf(`
+data "bastionzero_jit_policies" "test" {
+  filter_groups = %[1]s
+}
+`, acctest.ToTerraformStringList(groupIDs))
+}
