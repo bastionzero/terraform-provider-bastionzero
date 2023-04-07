8395f67d6aabcb5f2d07b988e18a657ab759d190
Yuval Marcus <ymarcus93@gmail.com>
2023-04-07 01:23:08 -0400

    Update env resource doc

---
+++ examples/resources/bastionzero_environment/import.sh
@@ -0,0 +1,2 @@
+# Environment can be imported by specifying the unique identifier.
+terraform import bastionzero_environment.example "01d7a020-2bbc-4ac0-b886-ac9e445d8ab1"
--- examples/resources/bastionzero_environment/resource.tf
+++ examples/resources/bastionzero_environment/resource.tf
@@ -1,5 +1,5 @@
-# Create a new environment
 resource "bastionzero_environment" "example" {
   name                          = "example-env"
+  description                   = "Example environment"
   offline_cleanup_timeout_hours = 12
 }
