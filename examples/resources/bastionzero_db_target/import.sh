79bd182d888a894360eb2c2433a99af80d60cc32
Yuval Marcus <ymarcus93@gmail.com>
2023-11-13 13:32:53 -0500

    Add some more docs/example

---
+++ examples/resources/bastionzero_db_target/import.sh
@@ -0,0 +1,2 @@
+# A Db target can be imported by specifying the unique identifier.
+terraform import bastionzero_db_target.example "01d7a020-2bbc-4ac0-b886-ac9e445d8ab1"
---
+++ examples/resources/bastionzero_db_target/splitcert.tf
@@ -0,0 +1,28 @@
+data "bastionzero_environments" "example" {}
+data "bastionzero_bzero_targets" "example" {}
+
+locals {
+  # Find environment with name "example-env". `env` is null if not found
+  env = one([
+    for each in data.bastionzero_environments.example.environments
+    : each if each.name == "example-env"
+  ])
+  # Find Bzero target with name "ubuntu". `proxy_target` is null if not found
+  proxy_target = one([
+    for each in data.bastionzero_bzero_targets.example.targets
+    : each if each.name == "ubuntu"
+  ])
+}
+
+resource "bastionzero_db_target" "example" {
+  name            = "example-splitcert-db-target"
+  remote_host     = "localhost"
+  environment_id  = local.env.id
+  remote_port     = 5432
+  proxy_target_id = local.proxy_target.id
+  database_authentication_config = {
+    authentication_type = "SplitCert"
+    database            = "Postgres"
+    label               = "Unmanaged Postgres"
+  }
+}
